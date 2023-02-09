ceb0e4bd682cbe8f50392a842559b882e4e30f76
CampusNET-Primero <primero@campusnet.es>
2023-02-09 17:42:46 +0100

    MineSweeper

---
+++ MineSweeper/Map.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MineSweeper
+{
+    internal class Map
+    {
+        public byte Width;
+        public byte Height;
+        public byte Mines;
+        public List<List<Casilla>> mapa = new List<List<Casilla>>();
+
+        public class Casilla
+        {
+            public bool Mine;
+            public int Distance;
+            public bool Visible;
+
+            public Casilla(bool mine)
+            {
+                Mine = mine;
+            }
+        }
+
+        public Map(byte width, byte height, byte mines)
+        {
+            Width = width;
+            Height = height;
+            Mines = mines;
+
+            for (int i = 0; i < Height; i++)
+            {
+                mapa.Add(new List<Casilla>());
+                for (int j = 0; j < Width; j++)
+                {
+                    mapa[i].Add(new Casilla(false));
+                }
+            }
+
+            Random rand = new Random();
+
+            for (int i = 0; i < Mines; i++)
+            {
+                int x = rand.Next(0, Width);
+                int y = rand.Next(0, Height);
+                mapa[y][x].Mine = true;
+            }
+
+            for (int y = 0; y < Height; y++)
+            {
+                for (int x = 0; x < Width; x++)
+                {
+                    try
+                    {
+                        if (mapa[y][x + 1].Mine)
+                        {
+                            mapa[y][x].Distance++;
+                        }
+                    }
+                    catch { }
+                    try
+                    {
+                        if (mapa[y][x - 1].Mine)
+                        {
+                            mapa[y][x].Distance++;
+                        }
+                    }
+                    catch { }
+                    try
+                    {
+                        if (mapa[y + 1][x].Mine)
+                        {
+                            mapa[y][x].Distance++;
+                        }
+                    }
+                    catch { }
+                    try
+                    {
+                        if (mapa[y - 1][x].Mine)
+                        {
+                            mapa[y][x].Distance++;
+                        }
+                    }
+                    catch { }
+                    try
+                    {
+                        if (mapa[y + 1][x + 1].Mine)
+                        {
+                            mapa[y][x].Distance++;
+                        }
+                    }
+                    catch { }
+                    try
+                    {
+                        if (mapa[y - 1][x - 1].Mine)
+                        {
+                            mapa[y][x].Distance++;
+                        }
+                    }
+                    catch { }
+                    try
+                    {
+                        if (mapa[y + 1][x - 1].Mine)
+                        {
+                            mapa[y][x].Distance++;
+                        }
+                    }
+                    catch { }
+                    try
+                    {
+                        if (mapa[y - 1][x + 1].Mine)
+                        {
+                            mapa[y][x].Distance++;
+                        }
+                    }
+                    catch { }
+                }
+            }
+        }
+
+        public void Draw()
+        {
+            for (int y = 0; y < Height; y++)
+            {
+                for (int x = 0; x < Width; x++)
+                {
+                    if (mapa[y][x].Visible)
+                    {
+                        if (mapa[y][x].Mine)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.BackgroundColor = ConsoleColor.Red;
+                            Console.Write('X');
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.BackgroundColor = ConsoleColor.Gray;
+                            if (mapa[y][x].Distance == 0)
+                            {
+                                Console.Write(' ');
+                            }
+                            else
+                            {
+                                Console.Write(mapa[y][x].Distance);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.BackgroundColor = ConsoleColor.DarkGray;
+                        Console.Write(' ');
+                    }
+
+                }
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.WriteLine();
+            }
+        }
+
+        public void Visibilizar()
+        {
+            for (int y = 0; y < Height; y++)
+            {
+                for (int x = 0; x < Width; x++)
+                {
+                    mapa[y][x].Visible = true;
+                }
+            }
+        }
+        public bool Visibilizar(byte X, byte Y)
+        {
+            if (mapa[Y][X].Visible) return false;
+            if (mapa[Y][X].Mine)
+            {
+                return true;
+            }
+            else
+            {
+                mapa[Y][X].Visible = true;
+                if (mapa[Y][X].Distance == 0)
+                {
+                    try { Visibilizar((byte)(Y - 1), (byte)(X)); } catch { }
+                    try { Visibilizar((byte)(Y + 1), (byte)(X)); } catch { }
+                    try { Visibilizar((byte)(Y), (byte)(X + 1)); } catch { }
+                    try { Visibilizar((byte)(Y), (byte)(X - 1)); } catch { }
+                    try { Visibilizar((byte)(Y - 1), (byte)(X + 1)); } catch { }
+                    try { Visibilizar((byte)(Y - 1), (byte)(X - 1)); } catch { }
+                    try { Visibilizar((byte)(Y + 1), (byte)(X + 1)); } catch { }
+                    try { Visibilizar((byte)(Y + 1), (byte)(X - 1)); } catch { }
+                }
+                return false;
+            }
+        }
+    }
+}
---
+++ MineSweeper/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace MineSweeper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine("Type the width of the map:");
+            byte width;
+            try
+            {
+                width = byte.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                width = 0;
+            }
+            if(width < 10) { width = 10; }
+            Console.WriteLine("Type the height of the map:");
+            byte height;
+            try
+            {
+                height = byte.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                height = 0;
+            }
+            if (height < 10) { height = 10; }
+            Console.WriteLine("How many mines do you want?");
+            byte mines;
+            try
+            {
+                mines = byte.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                mines = 0;
+            }
+            if (mines < 1) { mines = 1; }
+            Map mapa = new Map(width, height, mines);
+
+
+            bool run = true;
+
+            mapa.Draw();
+            while (run)
+            {
+                //Inputs
+                Tuple<byte, byte> coords = Inputs(mapa.Width, mapa.Height);
+                //Logica
+                if(mapa.Visibilizar(coords.Item1, coords.Item2))
+                {
+                    run = false;
+                    mapa.Visibilizar();
+                }
+                else
+                {
+
+                }
+                //Dibujado
+                Console.Clear();
+                mapa.Draw();
+            }
+
+        }
+
+
+        static Tuple<byte,byte> Inputs(byte width, byte height)
+        {
+            Console.WriteLine("Coord X:");
+            byte X;
+            try
+            {
+                X = byte.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                X = 0;
+            }
+            if (X >= width) { X = (byte)(width - 1); }
+            Console.WriteLine("Coord Y:");
+            byte Y;
+            try
+            {
+                Y = byte.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Y = 0;
+            }
+            if (Y >= height) { Y = (byte)(height - 1); }
+            return new Tuple<byte, byte>(X, Y);
+        }
+    }
+}
